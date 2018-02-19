5fbbbded11119e66a4fe5a71cf83bade4b4bc65e
Seyed Ali Ghaffari <36573923+aliteleco@users.noreply.github.com>
2018-02-19 06:40:43 +0330

    windows support
    now, this tool supports windows.

--- pinger.py
+++ pinger.py
@@ -29,32 +29,37 @@ def main():
             log_dest = sys.argv[2]      # log file name
 
         os_name = os.name
-        if os_name == 'posix':
-            #print 'Macos'
-            last_state = None
-            while True:
+        #print 'Macos'
+        last_state = None
+        while True:
+            if os_name == 'posix':
                 ping_res = os.popen('ping -c 1 {}'.format(ip)).read()
-                #print ping_res
-                for any_line in ping_res:
-                    if "time=" in ping_res:
-                        if last_state == False or last_state == None:
-                            write_log(datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S') + '-OK',log_dest)
-                        last_state = True
-                    else:
-                        if last_state == True:
-                            write_log(datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S') + '-NOK',log_dest)
-                        last_state = False
-                os.system('cls' if os.name == 'nt' else 'clear')
-                print "Pinger!! \nLast status is:"
-                
-                if last_state:
-                    print color.GREEN
-                    print "OK"
+            elif os_name == 'nt':
+                ping_res = os.popen('ping -n 1 {}'.format(ip)).read()
+            else:
+                print "your os cann't support :("
+                exit()
+            #print ping_res
+            for any_line in ping_res:
+                if "time=" in ping_res:
+                    if last_state == False or last_state == None:
+                        write_log(datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S') + '-OK',log_dest)
+                    last_state = True
                 else:
-                    print color.RED
-                    print "NOK"
-                print color.END
-                time.sleep(1)
+                    if last_state == True:
+                        write_log(datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S') + '-NOK',log_dest)
+                    last_state = False
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print "Pinger!! \nLast status is:"
+            
+            if last_state:
+                print color.GREEN
+                print "OK"
+            else:
+                print color.RED
+                print "NOK"
+            print color.END
+            time.sleep(1)
     else:
         print "Please type ip address :)"
 
